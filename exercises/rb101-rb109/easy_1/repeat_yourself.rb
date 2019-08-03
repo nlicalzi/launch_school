1b36cd67789c45eec737a9b45507853f540b3360
Nicholas LiCalzi <nicholas.licalzi@gmail.com>
2019-08-03 00:09:21 +0000

    Completed exercises/documentation_again

---
+++ exercises/rb101-rb109/easy_1/repeat_yourself.rb
@@ -0,0 +1,28 @@
+# Source: https://launchschool.com/exercises/a018e581
+
+# Question: Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates.
+
+# P: 
+#   Method accepts two arguments
+#     One argument is a string
+#     One argument is a positive integer
+#   Print `string` `integer` times
+# 
+# Should we assume that integers are clean? Always string first and then positive int?
+
+# E: 
+#   repeat('Hello', 3)
+#   repeat('Hello', 0)
+#   repeat('', 3)
+#   repeat('Hello')
+#   repeat(3)
+#   repeat(0, 'Hello')
+#   repeat(2, 3, 'Hello')
+#   repeat('Hello', -3)
+
+# D: 
+#   We will ingest one string and one integer
+#   We will print a string, x times
+
+# A:
+# 
