7c0a61d0e07aea5d8188d7eb91f9fdb4777ea0a1
Nicholas LiCalzi <nicholaslicalzi@gmail.com>
2019-10-30 17:44:31 -0400

    Completed Rubocop review of TicTacToe in RB101/Lesson_6

--- rb101_programming_foundations/lesson_6/tictactoe.rb
+++ rb101_programming_foundations/lesson_6/tictactoe.rb
@@ -1,15 +1,19 @@
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                [[1, 5, 9], [3, 5, 7]] # diagonals
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/AbcSize
 def display_board(hsh)
   system 'clear'
-  puts "You're a #{PLAYER_MARKER}. Computer is a #{COMPUTER_MARKER}."
   puts ""
+  puts "You're a #{PLAYER_MARKER}. Computer is a #{COMPUTER_MARKER}."
   puts "     |     |     "
   puts "  #{hsh[1]}  |  #{hsh[2]}  |  #{hsh[3]}  "
   puts "     |     |     "
@@ -23,6 +27,7 @@ def display_board(hsh)
   puts "     |     |     "
   puts ""
 end
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
@@ -60,17 +65,10 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
-                  [[1, 5, 9], [3, 5, 7]] # diagonals
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-          brd[line[1]] == COMPUTER_MARKER &&
-          brd[line[2]] == COMPUTER_MARKER
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
