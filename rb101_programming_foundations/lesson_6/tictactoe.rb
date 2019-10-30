08fb7b3f4b30ee5529ecb98280b61e0c5c21f984
Nicholas LiCalzi <nicholaslicalzi@gmail.com>
2019-10-30 16:02:31 -0400

    Completed Step 2 of TicTacToe in RB101/Lesson_6

--- rb101_programming_foundations/lesson_6/tictactoe.rb
+++ rb101_programming_foundations/lesson_6/tictactoe.rb
@@ -1,19 +1,51 @@
-def display_board
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
 
-  puts ""
-  puts "     |     |"
-  puts "     |     |"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
-  puts "     |     |"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
-  puts "     |     |"
-  puts "     |     |"
-  puts ""
-
+def prompt(msg)
+  puts "=> #{msg}"
 end
 
-display_board
+def display_board(hsh)
+  puts ""
+  puts "     |     |     "
+  puts "  #{hsh[1]}  |  #{hsh[2]}  |  #{hsh[3]}  "
+  puts "     |     |     "
+  puts "-----+-----+-----"
+  puts "     |     |     "
+  puts "  #{hsh[4]}  |  #{hsh[5]}  |  #{hsh[6]}  "
+  puts "     |     |     "
+  puts "-----+-----+-----"
+  puts "     |     |     "
+  puts "  #{hsh[7]}  |  #{hsh[8]}  |  #{hsh[9]}  "
+  puts "     |     |     "
+  puts ""
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_move!(brd)
+  square = ''
+  loop do
+    prompt "Please select a square (#{empty_squares(brd).join(', ')}):"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    p "Invalid input. Try again below."
+  end
+
+  brd[square] = PLAYER_MARKER
+end
+
+board = initialize_board
+display_board(board)
+
+player_move!(board)
+display_board(board)
