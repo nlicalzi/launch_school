2e9112d3edafe74386305f05d2a989b479ca391e
Nicholas LiCalzi <nicholaslicalzi@gmail.com>
2019-11-05 16:41:49 -0500

    Fixed gameplay looping logic, enabled replay. Next: track points...

--- rb101_programming_foundations/lesson_6/twenty_one.rb
+++ rb101_programming_foundations/lesson_6/twenty_one.rb
@@ -8,29 +8,28 @@ CARD_VALUE = { '2' => 2, '3' => 3, '4' => 4, '5' => 5,
                '10' => 10, 'jack' => 10, 'queen' => 10,
                'king' => 10, 'ace' => 'ace' }
 
-def prompt(msg)
-  puts "=> #{msg}"
+# GAMEPLAY METHODS
+
+def deal_card(hand, deck)
+  hand << deck.shuffle!.pop
 end
 
-def display_hands(player_hand, dealer_hand)
-  prompt "Player hand: #{player_hand}"
-  prompt "Dealer hand: #{dealer_hand[0]} & #{dealer_hand.size - 1} hidden."
-end
-
-def display_final_game_state(player_hand, dealer_hand)
-  system 'clear'
-  prompt "Player hand: #{player_hand}"
-  prompt "Player points: #{calculate_hand_value(player_hand)}"
-  prompt "Dealer hand: #{dealer_hand}"
-  prompt "Dealer points: #{calculate_hand_value(dealer_hand)}"
-end
-
-def summed_hand_value(hand)
-  hand.map { |card| CARD_VALUE[card[0]] }.sum
-end
-
+# BOOLEAN METHODS
 def hand_has_ace?(hand)
   hand.flatten.include?('ace')
+end
+
+def valid_player_choice?(input)
+  input.downcase == "hit" || input.downcase == "stay"
+end
+
+def busted?(hand)
+  calculate_hand_value(hand) > 21
+end
+
+# CALCULATION METHODS
+def summed_hand_value(hand)
+  hand.map { |card| CARD_VALUE[card[0]] }.sum
 end
 
 def calculate_aces_value(aceless_hand, aces)
@@ -49,16 +48,22 @@ def calculate_hand_value(hand)
   summed_hand_value(aceless_hand) + calculate_aces_value(aceless_hand, aces)
 end
 
-def valid_player_choice?(input)
-  input.downcase == "hit" || input.downcase == "stay"
+# DISPLAY METHODS
+def prompt(msg)
+  puts "=> #{msg}"
 end
 
-def busted?(hand)
-  calculate_hand_value(hand) > 21
+def display_hands(player_hand, dealer_hand)
+  prompt "Player hand: #{player_hand}"
+  prompt "Dealer hand: #{dealer_hand[0]} & #{dealer_hand.size - 1} hidden."
 end
 
-def deal_card(hand, deck)
-  hand << deck.shuffle!.pop
+def display_final_game_state(player_hand, dealer_hand)
+  system 'clear'
+  prompt "Player hand: #{player_hand}"
+  prompt "Player points: #{calculate_hand_value(player_hand)}"
+  prompt "Dealer hand: #{dealer_hand}"
+  prompt "Dealer points: #{calculate_hand_value(dealer_hand)}"
 end
 
 def display_results(player_hand, dealer_hand)
@@ -76,32 +81,29 @@ def display_winner(player_hand, dealer_hand)
   (player_pts > dealer_pts) || busted?(dealer_hand) ? ("Player") : ("Dealer")
 end
 
-deck = CARDS.product(SUITS_UNICODE) # An array w/ sub arrays: ['card', 'suit']
-
-dealer_hand = []
-dealer_hand_value = 0
-
-player_hand = []
-player_hand_value = 0
-
-system 'clear'
-prompt "Welcome to Twenty One! You'll be playing against the dealer."
-prompt "First, you'll each be dealt two cards."
-puts ""
-
-2.times do # deal 2 cards to the player and 2 to the dealer
-  deal_card(player_hand, deck)
-  deal_card(dealer_hand, deck)
-end
-
 loop do
+  deck = CARDS.product(SUITS_UNICODE) # An array w/ sub arrays: ['card', 'suit']
+
+  dealer_hand = [] # initialize our dealer's hand array
+  player_hand = [] # initialize our player's hand array
+
+  system 'clear'
+  prompt "Welcome to Twenty One! You'll be playing against the dealer."
+  prompt "First, you'll each be dealt two cards."
+  puts ""
+
+  2.times do # deal 2 cards to the player and 2 to the dealer
+    deal_card(player_hand, deck)
+    deal_card(dealer_hand, deck)
+  end
+
+  answer = nil
   loop do # player turn logic
     system 'clear'
     display_hands(player_hand, dealer_hand)
     prompt "You are holding #{calculate_hand_value(player_hand)} points. "
     prompt "Would you like to hit or stay?"
 
-    answer = ''
     loop do
       answer = gets.chomp
       break if valid_player_choice?(answer)
@@ -117,7 +119,6 @@ loop do
     system 'clear'
     display_final_game_state(player_hand, dealer_hand)
     prompt "Game over! Player busted, Dealer wins!"
-    break
   else # dealer turn logic
     system 'clear'
     display_hands(player_hand, dealer_hand)
@@ -131,5 +132,8 @@ loop do
     display_final_game_state(player_hand, dealer_hand)
     prompt "Game over! #{display_winner(player_hand, dealer_hand)} wins!"
   end
-  break
+
+  prompt "Would you like to play again? Enter (y) to continue..."
+  answer = gets.chomp
+  break unless answer.downcase.chr == "y"
 end
