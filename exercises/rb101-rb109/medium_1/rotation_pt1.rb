ac0739d7d47b5d1a47afb2abafabbd2d481acc59
Nicholas LiCalzi <nicholaslicalzi@gmail.com>
2019-11-12 16:58:50 -0500

    Completed Rotation pt 1 in Exercises/RB101/Medium_1

---
+++ exercises/rb101-rb109/medium_1/rotation_pt1.rb
@@ -0,0 +1,16 @@
+# Source: https://launchschool.com/exercises/4b2a26d5
+
+# Q: Write a method that rotates an array by moving the first element to the end
+#    of the array. The original array should not be modified.
+
+def rotate_array(arr)
+  arr[1..-1] + [arr[0]]
+end
+
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(['a']) == ['a']
+
+x = [1, 2, 3, 4]
+p rotate_array(x) == [2, 3, 4, 1]   # => true
+p x == [1, 2, 3, 4]                 # => true
