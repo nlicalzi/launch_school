ef7c1d9cb43ded5bc2cec59afe22fb6aa3a33e09
Nicholas LiCalzi <nicholaslicalzi@gmail.com>
2019-11-12 21:53:48 -0500

    Completed Further Exploration for Rotation Pt 1 in Exercises/RB101/Medium_1

--- exercises/rb101-rb109/medium_1/rotation_pt1.rb
+++ exercises/rb101-rb109/medium_1/rotation_pt1.rb
@@ -7,10 +7,26 @@ def rotate_array(arr)
   arr[1..-1] + [arr[0]]
 end
 
-p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
-p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
-p rotate_array(['a']) == ['a']
+def rotate_string(str)
+  arr = str.chars
+  arr_out = arr[1..-1] + [arr[0]]
+  arr_out.join
+end
 
-x = [1, 2, 3, 4]
-p rotate_array(x) == [2, 3, 4, 1]   # => true
-p x == [1, 2, 3, 4]                 # => true
+def rotate_integer(int)
+  arr = int.to_s.chars
+  arr_out = arr[1..-1] + [arr[0]]
+  arr_out.join.to_i
+end
+
+# p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+# p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+# p rotate_array(['a']) == ['a']
+#
+# x = [1, 2, 3, 4]
+# p rotate_array(x) == [2, 3, 4, 1]   # => true
+# p x == [1, 2, 3, 4]                 # => true
+
+p "Array method works? : #{rotate_array(%w(a b c)) == %w(b c a)}"
+p "String method works? : #{rotate_string('dachshund') == 'achshundd'}"
+p "Integer method works? : #{rotate_integer(123_456) == 234_561}"
