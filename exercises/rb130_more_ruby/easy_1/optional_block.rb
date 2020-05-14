97538045b1019d99b8b09c849c1bbf98c35fbb38
Nicholas LiCalzi <nicholaslicalzi@gmail.com>
2020-05-14 11:34:36 -0400

    completed exercise 2 from RB130/Easy_1

---
+++ exercises/rb130_more_ruby/easy_1/optional_block.rb
@@ -0,0 +1,9 @@
+# Source: https://launchschool.com/exercises/1e706bb0
+
+def compute
+  block_given? ? yield : "Does not compute."
+end
+
+puts compute { 5 + 3 } == 8
+puts compute { 'a' + 'b' } == 'ab'
+puts compute == 'Does not compute.'
