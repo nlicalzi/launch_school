0f1575ac0d63d719210c156924bdeb4be8982740
nlicalzi <nicholas.licalzi@gmail.com>
2020-05-16 18:12:58 -0400

    added test for CashRegister#accept_money

--- exercises/rb130_more_ruby/medium_testing/cashregister_test.rb
+++ exercises/rb130_more_ruby/medium_testing/cashregister_test.rb
@@ -7,5 +7,16 @@ require_relative 'transaction'
 
 
 class CashRegisterTest < MiniTest::Test
-  def setup; end
+  # def setup; end
+
+  def test_accept_money
+    register = CashRegister.new(0)
+    transaction = Transaction.new(20)
+    transaction.amount_paid = 20
+
+    assert_equal(register.total_money, 0)
+
+    register.accept_money(transaction)
+    assert_equal(register.total_money, 20)
+  end
 end
