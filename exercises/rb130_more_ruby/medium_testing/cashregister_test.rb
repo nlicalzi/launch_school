ba4ca1448c22dff81a7917a1f6aca5e87a0db8e6
nlicalzi <nicholas.licalzi@gmail.com>
2020-05-16 18:07:03 -0400

    initialized test file for CashRegister class

---
+++ exercises/rb130_more_ruby/medium_testing/cashregister_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+MiniTest::Reporters.use!
+
+require_relative 'cash_register'
+require_relative 'transaction'
+
+
+class CashRegisterTest < MiniTest::Test
+  def setup; end
+end
