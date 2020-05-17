f19f78a2a7fac94cb04304a6486db7a691cf0569
nlicalzi <nicholas.licalzi@gmail.com>
2020-05-17 11:48:26 -0400

    Series#slices now passes all tests, completed first pass

--- exercises/ruby_challenges/easy_1/series.rb
+++ exercises/ruby_challenges/easy_1/series.rb
@@ -3,3 +3,45 @@
 # Q: Write a program that will take a string of digits and give you all the
 # possible consecutive number series of length n in that string.
 
+# Problem
+# Given an input string, we are to return all possible "slices" of length n
+
+# Examples
+test_1 = "01234"
+
+# Data structure
+# We're appending results from each iteration to an array
+
+# Algo (pseudo code)
+# for a given string `str` and slice size `n`
+#   initialize an empty Array `result`
+#   initialize a counter `idx` to 0
+#   while idx < (str.size - n)
+#     result << str[idx..idx+n-1]
+#     idx += 1
+#   end
+#   return result
+
+# Code
+class Series
+  attr_reader :str
+
+  def initialize(str)
+    @str = str
+  end
+
+  def slices(n)
+    unless n <= str.size
+      raise ArgumentError.new("Slice size is too large.")
+    end
+
+    result = []
+    idx = 0
+
+    while idx < (str.size - n + 1)
+      result << str[idx..(idx+n-1)].chars.map(&:to_i)
+      idx += 1
+    end
+    result
+  end
+end
