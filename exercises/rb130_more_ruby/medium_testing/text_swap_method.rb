5e96f751a76c2c10ef4f3a3bd29459338833d798
nlicalzi <nicholas.licalzi@gmail.com>
2020-05-16 22:26:02 -0400

    completed exercise 9 in rb130/medium_testing

---
+++ exercises/rb130_more_ruby/medium_testing/text_swap_method.rb
@@ -0,0 +1,11 @@
+# source: https://launchschool.com/exercises/82427de8
+
+class Text
+  def initialize(text)
+    @text = text
+  end
+
+  def swap(letter_one, letter_two)
+    @text.gsub(letter_one, letter_two)
+  end
+end
