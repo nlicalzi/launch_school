8238b90a999e6bca3d1ae51769d917e967ea9f83
nlicalzi <nicholas.licalzi@gmail.com>
2020-05-16 22:10:33 -0400

    fixed issue where output was being printed as tests were run on Transaction#prompt_for_payment

--- exercises/rb130_more_ruby/medium_testing/transaction_test.rb
+++ exercises/rb130_more_ruby/medium_testing/transaction_test.rb
@@ -10,8 +10,9 @@ class TransactionTest < MiniTest::Test
 
   def test_prompt_for_payment
     transaction = Transaction.new(20)
-    input = StringIO.new('20\n')
-    transaction.prompt_for_payment(input: input)
+    input = StringIO.new('20')
+    output = StringIO.new
+    transaction.prompt_for_payment(input: input, output: output)
     assert_equal(20, transaction.amount_paid)
   end
 end
