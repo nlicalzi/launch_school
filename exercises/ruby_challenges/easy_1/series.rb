a66cab35ea09a46bcf3eaa389016a758cfd0f8b6
nlicalzi <nicholas.licalzi@gmail.com>
2020-05-17 11:14:36 -0400

    initialized files for exercise 1 in ruby_challenges/easy_1

---
+++ exercises/ruby_challenges/easy_1/series.rb
@@ -0,0 +1,5 @@
+# source: https://launchschool.com/exercises/3db163fb
+
+# Q: Write a program that will take a string of digits and give you all the
+# possible consecutive number series of length n in that string.
+
