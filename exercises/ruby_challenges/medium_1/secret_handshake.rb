5ad9528ebaf713d60b0ac93baa7c8acb444e139a
nlicalzi <nicholas.licalzi@gmail.com>
2020-05-20 14:40:02 -0400

    initialized files for exercise 1 in ruby_challenges/medium_1:

---
+++ exercises/ruby_challenges/medium_1/secret_handshake.rb
@@ -0,0 +1,5 @@
+# source: https://launchschool.com/exercises/997ac42a
+
+# Q: Write a program that will take a decimal number, and convert it to the
+# appropriate sequence of events for a secret handshake.
+
