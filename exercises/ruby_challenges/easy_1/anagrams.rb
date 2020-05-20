d8b8cc544010a69794fcc983ea28d61aeeeb2667
nlicalzi <nicholas.licalzi@gmail.com>
2020-05-20 10:41:50 -0400

    initialized files for exercise 6

---
+++ exercises/ruby_challenges/easy_1/anagrams.rb
@@ -0,0 +1,5 @@
+# source: https://launchschool.com/exercises/2fc03016
+
+# Q: Write a program that, given a word and a list of possible anagrams,
+# selects the correct sublist that contains the anagrams of the word.
+
