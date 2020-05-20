b026e52e186fc9da5b87d9acb1b03ba326ad09b7
nlicalzi <nicholas.licalzi@gmail.com>
2020-05-20 15:43:22 -0400

    moved pseudocode to bottom of file

--- exercises/ruby_challenges/medium_1/secret_handshake.rb
+++ exercises/ruby_challenges/medium_1/secret_handshake.rb
@@ -3,29 +3,6 @@
 # Q: Write a program that will take a decimal number, and convert it to the
 # appropriate sequence of events for a secret handshake.
 
-# 1 = wink
-# 10 = double blink
-# 100 = close your eyes
-# 1000 = jump
-# 10_000 = Reverse the order of operations in the handshake
-
-# Integer#to_s(2) converts a number to a string representation of its binary form
-# String#to_i(2) converts a binary string to an integer representation of its decimal form
-
-# - Create a class SecretHandshake that takes one argument `seed` on initialization
-#   - assign `@seed` to `seed.to_i`
-# - Create an instance method SecretHandshake#commands
-#   - If seed.to_i.zero? || seed.to_i.to_s != seed, return []
-#   - create output array `out`
-#   - split @seed into its digits and reverse their order
-#     - out << 'wink' if @seed[0] == 1
-#     - out << 'double blink' if @seed[1] == 1
-#     - out << 'close your eyes' if @seed[2] == 1
-#     - out << 'jump' if @seed[3] == 1
-#     - out.reverse! if @seed[4] == 1
-#   - return out
-# C
-
 class SecretHandshake
   def initialize(val)
     @seed = valid_int?(val) && valid_bool?(val) ? val.to_i : 0
@@ -57,3 +34,27 @@ class SecretHandshake
     val.to_i.to_s(2).chars.all? { |char| %w(0 1).include?(char) }
   end
 end
+
+
+# Pseudocode
+# 1 = wink
+# 10 = double blink
+# 100 = close your eyes
+# 1000 = jump
+# 10_000 = Reverse the order of operations in the handshake
+
+# Integer#to_s(2) converts a number to a string representation of its binary form
+# String#to_i(2) converts a binary string to an integer representation of its decimal form
+
+# - Create a class SecretHandshake that takes one argument `seed` on initialization
+#   - assign `@seed` to `seed.to_i`
+# - Create an instance method SecretHandshake#commands
+#   - If seed.to_i.zero? || seed.to_i.to_s != seed, return []
+#   - create output array `out`
+#   - split @seed into its digits and reverse their order
+#     - out << 'wink' if @seed[0] == 1
+#     - out << 'double blink' if @seed[1] == 1
+#     - out << 'close your eyes' if @seed[2] == 1
+#     - out << 'jump' if @seed[3] == 1
+#     - out.reverse! if @seed[4] == 1
+#   - return out
