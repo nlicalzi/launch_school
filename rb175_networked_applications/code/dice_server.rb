c6a495fe9463c01bf5cd27ddf90ee22de0e0e115
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-07 17:14:11 -0400

    initialized file for Parsing Path and Parameters

---
+++ rb175_networked_applications/code/dice_server.rb
@@ -0,0 +1,16 @@
+require "socket"
+
+server = TCPServer.new("localhost", 3003)
+loop do
+  client = server.accept
+
+  request_line = client.gets
+  puts request_line
+  
+  client.puts "HTTP/1.1 200 OK"                   # needed for rendering in Chrome
+  client.puts "Content-Type: text/plain\r\n\r\n"  # needed for rendering in Chrome
+  client.puts request_line
+  client.puts rand(6) + 1
+
+  client.close
+end
