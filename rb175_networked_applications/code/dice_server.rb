cd24e419993519c80a48c865185e82f9f66e8efb
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-07 17:45:12 -0400

    added logic for parsing rolls and sides parameters and rendering results of rolls

--- rb175_networked_applications/code/dice_server.rb
+++ rb175_networked_applications/code/dice_server.rb
@@ -7,11 +7,20 @@ loop do
   request_line = client.gets
   next if !request_line || request_line =~ /favicon/
   puts request_line
+
+  http_method = request_line.split.first
+
+  path, query_parameters = request_line.split[1].split("?")
+
+  params = {}
+  query_parameters.split("&")\
+                  .map { |param| param.split("=")}\
+                  .each { |k, v| params[k] = v }
   
   client.puts "HTTP/1.1 200 OK"                   # needed for rendering in Chrome
   client.puts "Content-Type: text/plain\r\n\r\n"  # needed for rendering in Chrome
-  client.puts request_line
-  client.puts rand(6) + 1
+
+  params["rolls"].to_i.times { |_| client.puts rand(params["sides"].to_i) + 1 }
 
   client.close
 end
