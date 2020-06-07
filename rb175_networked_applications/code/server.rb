3d2812196ac9fcc31519acfec0d88ac7b23490f8
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-07 17:13:54 -0400

    added comments

--- rb175_networked_applications/code/server.rb
+++ rb175_networked_applications/code/server.rb
@@ -7,8 +7,8 @@ loop do
   request_line = client.gets
   puts request_line
   
-  client.puts "HTTP/1.1 200 OK"
-  client.puts "Content-Type: text/plain\r\n\r\n"
+  client.puts "HTTP/1.1 200 OK"                   # needed for rendering in Chrome
+  client.puts "Content-Type: text/plain\r\n\r\n"  # needed for rendering in Chrome
   client.puts request_line
   client.close
 end
