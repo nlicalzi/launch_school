69b09919cc94815dacd29cdcf9ba1f4e803bc8f1
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-07 21:15:48 -0400

    refactored code, extracted logic to parse_request method

--- rb175_networked_applications/code/dice_server.rb
+++ rb175_networked_applications/code/dice_server.rb
@@ -1,5 +1,17 @@
 require "socket"
 
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split(" ")
+  path, params = path_and_params.split("?")
+
+  params = params.split("&").each_with_object({}) do |pair, hsh|
+    key, value = pair.split("=")
+    hsh[key] = value
+  end
+
+  [http_method, path, params]
+end
+
 server = TCPServer.new("localhost", 3003)
 loop do
   client = server.accept
@@ -8,19 +20,19 @@ loop do
   next if !request_line || request_line =~ /favicon/
   puts request_line
 
-  http_method = request_line.split.first
+  http_method, path, params = parse_request(request_line)
 
-  path, query_parameters = request_line.split[1].split("?")
-
-  params = {}
-  query_parameters.split("&")\
-                  .map { |param| param.split("=")}\
-                  .each { |k, v| params[k] = v }
+  rolls = params["rolls"].to_i
+  sides = params["sides"].to_i
   
   client.puts "HTTP/1.1 200 OK"                   # needed for rendering in Chrome
   client.puts "Content-Type: text/plain\r\n\r\n"  # needed for rendering in Chrome
 
-  params["rolls"].to_i.times { |_| client.puts rand(params["sides"].to_i) + 1 }
+  client.puts params
+  rolls.times do 
+    roll = rand(sides) + 1
+    client.puts roll
+  end
 
   client.close
 end
