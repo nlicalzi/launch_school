f8a519301fb5dda7a7781de68081ff2fde8d99fd
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-07 21:47:32 -0400

    added links for incrementing and decrementing the number parameter

--- rb175_networked_applications/code/counter_server.rb
+++ rb175_networked_applications/code/counter_server.rb
@@ -4,7 +4,7 @@ def parse_request(request_line)
   http_method, path_and_params, http = request_line.split(" ")
   path, params = path_and_params.split("?")
 
-  params = params.split("&").each_with_object({}) do |pair, hsh|
+  params = (params || "").split("&").each_with_object({}) do |pair, hsh|
     key, value = pair.split("=")
     hsh[key] = value
   end
@@ -17,18 +17,26 @@ loop do
   client = server.accept
 
   request_line = client.gets
-  next if !request_line || request_line =~ /favicon/
+  next if !request_line || request_line =~ /favicon/  # skip if favicon error msg
+
   http_method, path, params = parse_request(request_line)
   
-  client.puts "HTTP/1.1 200 OK"                   # needed for rendering in Chrome
-  client.puts "Content-Type: text/html\r\n\r\n"   # needed for rendering in Chrome
-  client.puts                                     # empty line btw. header and body
+  client.puts "HTTP/1.1 200 OK"                       # needed for rendering in Chrome
+  client.puts "Content-Type: text/html\r\n\r\n"       # needed for rendering in Chrome
+  client.puts                                         # empty line btw. header and body
   client.puts "<html>"
   client.puts "<body>"
   client.puts "<pre>"
-  client.puts "#{http_method} #{path} #{params}"
+  client.puts "#{http_method}\n#{path}\n#{params}"
   client.puts "</pre>"
 
+  client.puts "<h1>Counter</h1>"
+
+  number = params["number"].to_i
+  client.puts "<p>The current number is #{number}.</p>"
+
+  client.puts "<a href='?number=#{number - 1}'>Subtract one</a>"
+  client.puts "<a href='?number=#{number + 1}'>Add one</a>"
   client.puts "</body>"
   client.puts "</html>"
   
