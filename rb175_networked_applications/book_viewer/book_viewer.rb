5c59e679a1eee155814fa56d0cdcab89b6f47089
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-09 13:32:26 -0400

    included Before statement to load contents only once

--- rb175_networked_applications/book_viewer/book_viewer.rb
+++ rb175_networked_applications/book_viewer/book_viewer.rb
@@ -2,20 +2,21 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.read("data/toc.txt").split("\n")
 
   erb :home
 end
 
 get "/chapters/:number" do
-  @contents = File.readlines("data/toc.txt")
-
   number = params[:number].to_i
   chapter_name = @contents[number - 1]
+  
   @title = "Chapter #{number}: #{chapter_name}"
-
   @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
