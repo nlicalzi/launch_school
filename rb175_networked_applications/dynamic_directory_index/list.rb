c3148fef6c5e2342be5765ef118644767b600a87
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-09 11:27:15 -0400

    completed Dynamic Directory Index code challenge

---
+++ rb175_networked_applications/dynamic_directory_index/list.rb
@@ -0,0 +1,10 @@
+require "tilt/erubis"
+require "sinatra"
+require "sinatra/reloader"
+
+get "/" do
+  @files = Dir.glob("public/*").map { |file| File.basename(file) }.sort
+  @files.reverse! if params[:sort] == "desc"
+
+  erb :list
+end
