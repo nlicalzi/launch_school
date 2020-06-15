af01f83d1bffc64c18b6166bb7314152b8ce81c1
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-15 16:21:57 -0400

    added an index page

--- rb175_networked_applications/cms/cms.rb
+++ rb175_networked_applications/cms/cms.rb
@@ -3,6 +3,9 @@ require "sinatra/reloader"
 require "sinatra/content_for"
 require "tilt/erubis"
 
+root = File.expand_path("..", __FILE__)
+
 get "/" do
-  "Getting started."
+  @files = Dir.glob(root + "/data/*").map { |path| File.basename(path) }
+  erb :index
 end
