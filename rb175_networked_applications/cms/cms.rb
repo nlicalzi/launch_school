16dda6a88e63b643d670a1b47b60f668182b9148
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-15 17:11:10 -0400

    implemented ability to read individual files

--- rb175_networked_applications/cms/cms.rb
+++ rb175_networked_applications/cms/cms.rb
@@ -5,7 +5,15 @@ require "tilt/erubis"
 
 root = File.expand_path("..", __FILE__)
 
+# load index page w/ all files
 get "/" do
   @files = Dir.glob(root + "/data/*").map { |path| File.basename(path) }
   erb :index
+end
+
+# return individual plaintext file resources
+get "/:resource" do
+  file_path = root + "/data/" + params[:resource]
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
 end
