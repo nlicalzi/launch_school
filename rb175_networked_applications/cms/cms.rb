2426ab5d9224cbc7e18687db6e82cef2c70ed823
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-16 11:52:43 -0400

    changed :resource to :filename in paths and params

--- rb175_networked_applications/cms/cms.rb
+++ rb175_networked_applications/cms/cms.rb
@@ -10,6 +10,13 @@ end
 
 root = File.expand_path("..", __FILE__)
 
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+end
+
 def render_markdown(text)
   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   markdown.render(text)
@@ -28,31 +35,32 @@ end
 
 # load index page w/ all files
 get "/" do
-  @files = Dir.glob(root + "/data/*").map { |path| File.basename(path) }
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map { |path| File.basename(path)}
   erb :index
 end
 
 # return individual plaintext file resources
-get "/:resource" do
-  file_path = root + "/data/" + params[:resource]
+get "/:filename" do
+  file_path = File.join(data_path, params[:filename])
 
   if File.file?(file_path)
     load_file_content(file_path)
   else
-    session[:message] = "#{params[:resource]} does not exist."
+    session[:message] = "#{params[:filename]} does not exist."
     redirect "/"
   end
 end
 
 # file editing page
-get "/:resource/edit" do
-  @file_name = params[:resource]
+get "/:filename/edit" do
+  @file_name = params[:filename]
   file_path = root + "/data/" + @file_name
 
   if File.file?(file_path)
     @content = File.read(file_path)
   else
-    session[:message] = "#{params[:resource]} does not exist."
+    session[:message] = "#{params[:filename]} does not exist."
     redirect "/"
   end
 
@@ -60,11 +68,11 @@ get "/:resource/edit" do
 end
 
 # endpoint for editing a file
-post "/:resource" do
-  file_path = root + "/data/" + params[:resource]
+post "/:filename" do
+  file_path = root + "/data/" + params[:filename]
 
   File.write(file_path, params[:content])
 
-  session[:message] = "#{params[:resource]} has been updated."
+  session[:message] = "#{params[:filename]} has been updated."
   redirect "/"
 end
