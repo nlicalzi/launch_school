49b69b3fb4dc86e8fb87ef7f3caffb7fbea7154b
nlicalzi <nicholas.licalzi@gmail.com>
2020-09-13 20:19:08 -0400

    moving all assessment notes and study guides out of git tracking

--- rb101_programming_foundations/rb109_general_ruby_assessment/exercises/101-109_review/easy/even_numbers.rb
+++
@@ -1,3 +0,0 @@
-# Source: https://launchschool.com/exercises/e3801a1a
-
-(1..99).to_a.each { |x| puts x if x.even? }
--- rb101_programming_foundations/rb109_general_ruby_assessment/exercises/101-109_review/easy/greeting_a_user.rb
+++
@@ -1,9 +0,0 @@
-# Source: https://launchschool.com/exercises/1af9d2f7
-
-# Write a program that will ask for user's name. The program will then greet
-# the user. If the user writes "name!" then the computer yells back to the user.
-
-print "What is your name? "
-name = gets.chomp
-
-puts name[-1] == '!' ? "HELLO #{name[0..-2].upcase}. WHY ARE WE SCREAMING?" : "Hello #{name}."
