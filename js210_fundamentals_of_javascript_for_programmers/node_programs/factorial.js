ba6c329d0907301c146eb4037fa58e138189cc3a
nlicalzi <nicholas.licalzi@gmail.com>
2020-09-28 20:27:08 -0400

    completed exercises for Loops & Iterating

--- js210_fundamentals_of_javascript_for_programmers/node_programs/factorial.js
+++ js210_fundamentals_of_javascript_for_programmers/node_programs/factorial.js
@@ -1,11 +1,14 @@
-function times(number1, number2) {
-  let result = number1 * number2;
-  console.log(result);
-  return result;
+function factorial(number) {
+  if (number <= 1) return 1;
+  return number * factorial(number - 1);
 }
 
-let oneFactorial = times(1, 1);
-let twoFactorial = times(2, oneFactorial);
-let threeFactorial = times(3, twoFactorial);
-let fourFactorial = times(4, threeFactorial);
-let fiveFactorial = times(5, fourFactorial);
+console.log(factorial(0));     // => 1
+console.log(factorial(1));     // => 1
+console.log(factorial(2));     // => 2
+console.log(factorial(3));     // => 6
+console.log(factorial(4));     // => 24
+console.log(factorial(5));     // => 120
+console.log(factorial(6));     // => 720
+console.log(factorial(7));     // => 5040
+console.log(factorial(8));     // => 40320
