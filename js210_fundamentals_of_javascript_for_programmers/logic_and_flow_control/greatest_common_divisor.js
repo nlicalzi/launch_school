5ef5394e3dd4510318307c3ace1bf73fd831043a
nlicalzi <nicholas.licalzi@gmail.com>
2020-10-02 10:34:57 -0400

    added reference implementation for Greatest Common Divisor

--- js210_fundamentals_of_javascript_for_programmers/logic_and_flow_control/greatest_common_divisor.js
+++ js210_fundamentals_of_javascript_for_programmers/logic_and_flow_control/greatest_common_divisor.js
@@ -10,6 +10,18 @@ function gcd(val1, val2) {
   return 1;
 }
 
+// function gcd(num1, num2) {
+//   var temp;
+
+//   while (num2 !== 0) {
+//     temp = num2;
+//     num2 = num1 % num2;
+//     num1 = temp;
+//   }
+
+//   return num1;
+// }
+
 console.log(gcd(12, 4));   // 4
 console.log(gcd(15, 10));  // 5
 console.log(gcd(9, 2));    // 1
