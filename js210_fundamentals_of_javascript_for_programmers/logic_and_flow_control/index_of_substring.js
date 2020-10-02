fce19c8c547da3b177f21cbd1d1df1e26249690b
nlicalzi <nicholas.licalzi@gmail.com>
2020-10-02 13:35:12 -0400

    added reference implementation of Index of Substring

---
+++ js210_fundamentals_of_javascript_for_programmers/logic_and_flow_control/index_of_substring.js
@@ -0,0 +1,48 @@
+function indexOf(firstString, secondString) {
+  let limit = firstString.length - secondString.length;
+
+  for (let indexFirst = 0; indexFirst <= limit; indexFirst += 1) {
+    let matchCount = 0;
+
+    for (let indexSecond = 0; indexSecond < secondString.length; indexSecond += 1) {
+      if (firstString[indexFirst + indexSecond] === secondString[indexSecond]) {
+        matchCount += 1;
+      } else {
+        break;
+      }
+    }
+
+    if (matchCount === secondString.length) {
+      return indexFirst;
+    }
+  }
+
+  return -1;
+}
+
+function lastIndexOf(firstString, secondString) {
+  let limit = firstStirng.length - secondString.length;
+
+  for (let indexFirst = limit; indexFirst >= 0; indexFirst -= 1) {
+    let matchCount = 0;
+
+    for (let indexSecond = 0; indexSecond < secondString.length; indexSecond += 1) {
+      if (firstString[indexFirst + indexSecond] === secondString[indexSecond]) {
+        matchCount += 1;
+      } else {
+        break;
+      }
+    }
+
+    if (matchCount === secondString.length) {
+      return indexFirst;
+    }
+  }
+
+  return -1;
+}
+
+console.log(indexOf('Some strings', 's'));                      // 5
+console.log(indexOf('Blue Whale', 'Whale'));                    // 5
+console.log(indexOf('Blue Whale', 'Blute'));                    // -1
+console.log(indexOf('Blue Whale', 'leB'));                      // -1
