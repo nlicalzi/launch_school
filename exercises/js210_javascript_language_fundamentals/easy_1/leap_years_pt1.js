545bd64eebff3b2137656ac8fc37d5ed23d2df47
nlicalzi <nicholas.licalzi@gmail.com>
2020-10-04 18:05:12 -0400

    refactored Leap Year pt1

--- exercises/js210_javascript_language_fundamentals/easy_1/leap_years_pt1.js
+++ exercises/js210_javascript_language_fundamentals/easy_1/leap_years_pt1.js
@@ -1,13 +1,11 @@
 function isLeapYear(year) {
-if (year % 400 === 0) {
-  return true;
-} else if (year % 100 === 0) {
-  return false;
-} else if (year % 4 === 0) {
-  return true;
-} else {
-  return false;
-}
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else {
+    return (year % 4 === 0);
+  }
 }
 
 console.log(isLeapYear(2016) === true);      // true
