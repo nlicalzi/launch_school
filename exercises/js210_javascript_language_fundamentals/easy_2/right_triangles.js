65d5f3e4c2d89615d05cdfc88306069a2311fba9
nlicalzi <nicholas.licalzi@gmail.com>
2020-10-04 21:35:37 -0400

    refactored Right Triangles

--- exercises/js210_javascript_language_fundamentals/easy_2/right_triangles.js
+++ exercises/js210_javascript_language_fundamentals/easy_2/right_triangles.js
@@ -1,5 +1,4 @@
 function triangle(num) {
-  let stars = 1;
   for (let stars = 1; stars <= num; stars += 1) {
     let spaces = num - stars;
     console.log(' '.repeat(spaces) + '*'.repeat(stars));
