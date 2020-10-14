90538631d4880c23f66e1de0bc278cc8703b23ae
nlicalzi <nicholas.licalzi@gmail.com>
2020-10-13 20:15:15 -0400

    solved Amount Payable from JS210/Medium_2

---
+++ exercises/js210_javascript_language_fundamentals/medium_2/amount_payable.js
@@ -0,0 +1,13 @@
+let startingBalance = 1;
+const chicken = 5;
+const chickenQuantity = 7;
+
+function totalPayable(item, quantity) {
+  return startingBalance + (item * quantity);
+}
+
+startingBalance = 5;
+console.log(totalPayable(chicken, chickenQuantity));
+
+startingBalance = 10;
+console.log(totalPayable(chicken, chickenQuantity));
