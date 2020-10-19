82f1b43636176465198d19ccfd0cd47f3a8a6bf4
nlicalzi <nicholas.licalzi@gmail.com>
2020-10-19 13:56:08 -0400

    added solution to Practice Problem: Processing Releases

---
+++ js215_computational_thinking/list_processing_and_functional_abstractions/process_release_data.js
@@ -0,0 +1,48 @@
+function processReleaseData(releases) {
+  // filter for releases having both release[id] and release[title]
+    // we can assume that id and title, when existing, are non-0/non-empty string
+  // return an array of objects containing id and title key/value pairs
+  return releases.filter(release => release['id'] && release['title'])
+                 .map(release => {
+                   return {
+                     id: release.id,
+                     title: release.title,
+                   };
+                 });
+}
+
+let newReleases = [
+  {
+    'id': 70111470,
+    'title': 'Die Hard',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/DieHard.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [4.0],
+    'bookmark': [],
+  },
+  {
+    'id': 654356453,
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/BadBoys.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [5.0],
+    'bookmark': [{ id:432534, time:65876586 }],
+  },
+  {
+    'title': 'The Chamber',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/TheChamber.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [4.0],
+    'bookmark': [],
+  },
+  {
+    'id': 675465,
+    'title': 'Fracture',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/Fracture.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [5.0],
+    'bookmark': [{ id:432534, time:65876586 }],
+  },
+];
+
+console.log(processReleaseData(newReleases));
+// [{ id: 70111470, title: 'Die Hard'}, { id: 675465, title: 'Fracture' }];
