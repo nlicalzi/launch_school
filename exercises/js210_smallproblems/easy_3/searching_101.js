b4493886c7ec25dd434313154bfe313b272f08b5
nlicalzi <nicholas.licalzi@gmail.com>
2020-10-26 16:43:21 -0400

    completed Searching 101 in JS210/Easy_3

--- exercises/js210_smallproblems/easy_3/searching_101.js
+++ exercises/js210_smallproblems/easy_3/searching_101.js
@@ -4,11 +4,19 @@
 function searching101() {
   var readlineSync = require('readline-sync');
 
-  let times = ['1st', '2nd', '3rd', '4th', '5th', 'last']
+  let times = ['1st', '2nd', '3rd', '4th', '5th', 'last'];
+  let inputs = [];
   
   times.forEach(time => {
-    readlineSync.question(`Enter the ${time} number: `);
+    inputs.push(readlineSync.question(`Enter the ${time} number: `));
   });
+
+  let final = inputs.pop();
+  if (inputs.includes(final)) {
+    return `The number ${final} appears in [${inputs.join(', ')}].`
+  } else {
+    return `The number ${final} does not appear in [${inputs.join(', ')}].`
+  }
 }
 
-searching101();
+console.log(searching101());
