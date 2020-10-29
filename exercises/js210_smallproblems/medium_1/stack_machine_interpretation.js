f899b939fe831b551c5baef12c373f4eddb257f3
nlicalzi <nicholas.licalzi@gmail.com>
2020-10-29 17:51:32 -0400

    solved Stack Machine Interpretation in JS210/Medium1

---
+++ exercises/js210_smallproblems/medium_1/stack_machine_interpretation.js
@@ -0,0 +1,49 @@
+// See description here: https://launchschool.com/exercises/6d0a9bf4
+
+function minilang(program) {
+  let register = 0;
+  let stack = [];
+
+  let commands = program.split(' ');
+  commands.forEach(command => {
+    switch (command) {
+      case 'PUSH':
+        stack.push(register);
+        break;
+      case 'ADD':
+        register += stack.pop();
+        break;
+      case 'SUB':
+        register -= stack.pop();
+        break;
+      case 'MULT':
+        register *= stack.pop();
+        break;
+      case 'DIV':
+        register = Math.floor(register / stack.pop());
+        break;
+      case 'MOD':
+        register = Math.floor(register % stack.pop());
+        break;
+      case 'POP':
+        register = stack.pop();
+        break;
+      case 'PRINT':
+        console.log(register);
+        break;
+      default:
+        register = parseInt(command, 10);
+    }
+  });
+}
+
+minilang('PRINT');
+// 0
+
+minilang('5 PUSH 3 MULT PRINT');
+// 15
+
+minilang('5 PRINT PUSH 3 PRINT ADD PRINT');
+// 5
+// 3
+// 8
