d3e6a7cd1b9cb733244efe3405b2449a351be7dc
nlicalzi <nicholas.licalzi@gmail.com>
2020-10-29 22:02:48 -0400

    added solution to Word to Digit in JS210/Medium1

---
+++ exercises/js210_smallproblems/medium_1/word_to_digit.js
@@ -0,0 +1,15 @@
+// Write a function that takes a sentence string as an argument, and returns that string
+// with every occurrence of a "number word" — 'zero', 'one', 'two', 'three', 'four',
+// 'five', 'six', 'seven', 'eight', 'nine' — converted to its corresponding digit character.
+
+function wordToDigit(sentence) {
+  const numWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  numWords.forEach(word => {
+    let regex = new RegExp(word, 'g');
+    sentence = sentence.replace(regex, numWords.indexOf(word));
+  });
+  return sentence;
+}
+
+console.log(wordToDigit('Please call me at five five five one two three four. Thanks.'));
+// "Please call me at 5 5 5 1 2 3 4. Thanks."
