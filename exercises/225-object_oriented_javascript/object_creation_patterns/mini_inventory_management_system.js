abffeb94d7516eaabbb54b26aec7c9d6fb965f2c
nlicalzi <nicholas.licalzi@gmail.com>
2020-11-26 23:44:31 -0500

    initialized solution for Mini Inventory Management System in 225

---
+++ exercises/225-object_oriented_javascript/object_creation_patterns/mini_inventory_management_system.js
@@ -0,0 +1,11 @@
+// https://launchschool.com/exercises/d6d3971a
+
+// Build a simple inventory management system.
+// The system is composed of an item creator, an items manager, and a reports manager.
+// The item creator makes sure that all necessary information are present and valid.
+// The item manager is responsible for creating items, updating information about items,
+// deleting items, and querying information about the items.
+// Finally, the report manager generates reports for a specific item or ALL items.
+
+// Reports for specific items are generated from report objects created from the report
+// manager. The report manager is responsible for reports for all items.
