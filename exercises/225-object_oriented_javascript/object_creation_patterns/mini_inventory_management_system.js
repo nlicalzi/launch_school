7ec08a9da31d6d0498291d8a60113d9e70ccdcec
nlicalzi <nicholas.licalzi@gmail.com>
2020-11-30 16:07:58 -0500

    added notes for each required class

--- exercises/225-object_oriented_javascript/object_creation_patterns/mini_inventory_management_system.js
+++ exercises/225-object_oriented_javascript/object_creation_patterns/mini_inventory_management_system.js
@@ -11,13 +11,33 @@
 // manager. The report manager is responsible for reports for all items.
 
 function ItemCreator() {
-  
+  // PROPERTIES
+    // SKU code (UID/str):
+      // first 3 letters of item name (trim spaces), first 2 letters of category
+    // Item Name (str):
+      // min 5 non-space characters
+    // Category (str):
+      // min 5 chars, no spaces allowed
+    // Quantity (num):
+      // required value, assume valid number provided
 }
 
 function ItemManager() {
-
+  // METHODS
+    // create(), creates a new item (returns false if creation not successful)
+    // update(SKU, obj), updates any of the info on an item (assume valid values)
+    // delete(SKU), deletes the item from the list (assume valid SKU)
+    // items(), return all items
+    // inStock(), return all items with quantity > 0
+    // itemsInCategory(category), list all items for given category
 }
 
 function ReportManager() {
-
+  // PROPERTIES
+    // items, should reference an itemManager object
+  // METHODS
+    // init(itemManager), assigns itemManager to `items` property
+    // createReporter(SKU), returns an object having:
+      // method itemInfo(), log all properties of obj as k:v pairs, one per line
+    // reportInStock(), log all items in stock as comma separated values
 }
