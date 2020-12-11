a65d323e8841d8f18e36704520378ce75c81eb67
nlicalzi <nicholas.licalzi@gmail.com>
2020-12-11 12:00:21 -0500

    completed Part 3 of Autocomplete project

--- 230-DOM_and_asynchronous_programming/making_HTTP_requests_from_JS/autocomplete/public/javascripts/autocomplete.js
+++ 230-DOM_and_asynchronous_programming/making_HTTP_requests_from_JS/autocomplete/public/javascripts/autocomplete.js
@@ -20,15 +20,89 @@ const Autocomplete = {
     this.overlay = overlay;
   },
 
+  bindEvents: function() {
+    this.input.addEventListener('input', this.valueChanged.bind(this));
+  },
+
+  valueChanged: function() {
+    let value = this.input.value;
+
+    if (value.length > 0) {
+      this.fetchMatches(value, matches => {
+        this.visible = true;
+        this.matches = matches;
+        this.bestMatchIndex = 0;
+        this.draw();
+      });
+    } else {
+      this.reset();
+    }
+  },
+
+  // communicate with server
+  fetchMatches: function(query, callback) {
+    let request = new XMLHttpRequest();
+
+    request.addEventListener('load', () => {
+      callback(request.response);
+    });
+
+    request.open('GET', `${this.url}${encodeURIComponent(query)}`);
+    request.responseType = 'json';
+    request.send();
+  },
+
+  // rendering list of countries
+  draw: function() {
+    while (this.listUI.lastChild) {
+      this.listUI.removeChild(this.listUI.lastChild);
+    }
+
+    if (!this.visible) {
+      this.overlay.textContent = '';
+      return;
+    }
+
+    if (this.bestMatchIndex !== null && this.matches.length !== 0) {
+      let selected = this.matches[this.bestMatchIndex];
+      this.overlay.textContent = this.generateOverlayContent(this.input.value, selected);
+      this.overlay.textContent = selected.name;
+    } else {
+      this.overlay.textContent = '';
+    }
+
+    this.matches.forEach(match => {
+      let li = document.createElement('li');
+      li.classList.add('autocomplete-ui-choice');
+
+      li.textContent = match.name;
+      this.listUI.appendChild(li);
+    });
+  },
+
+  reset: function() {
+    this.visible = false;
+    this.matches = [];
+    this.bestMatchIndex = null;
+
+    this.draw();
+  },
+
   init: function() {
     this.input = document.querySelector('input');
     this.url = '/countries?matching=';
 
-    this.listUI = null;   // explicitly set initial state
-    this.overlay = null;  // explicitly set initial state
+    // explicitly set initial states
+    this.listUI = null;
+    this.overlay = null;
+    this.visible = false;
+    this.matches = []
 
     this.wrapInput();
     this.createUI();
+    this.bindEvents();
+
+    this.reset();
   }
 };
 
