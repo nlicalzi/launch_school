c856d7a434baffb8562594e30a1ea8677019c555
nlicalzi <nicholas.licalzi@gmail.com>
2020-12-11 14:04:07 -0500

    completed part 4 of Autocomplete project

--- 230-DOM_and_asynchronous_programming/making_HTTP_requests_from_JS/autocomplete/public/javascripts/autocomplete.js
+++ 230-DOM_and_asynchronous_programming/making_HTTP_requests_from_JS/autocomplete/public/javascripts/autocomplete.js
@@ -22,16 +22,20 @@ const Autocomplete = {
 
   bindEvents: function() {
     this.input.addEventListener('input', this.valueChanged.bind(this));
+    this.input.addEventListener('keydown', this.handleKeydown.bind(this));
+    this.listUI.addEventListener('mousedown', this.handleMousedown.bind(this));
   },
 
   valueChanged: function() {
     let value = this.input.value;
+    this.previousValue = value;
 
     if (value.length > 0) {
       this.fetchMatches(value, matches => {
         this.visible = true;
         this.matches = matches;
         this.bestMatchIndex = 0;
+        this.selectedIndex = null;
         this.draw();
       });
     } else {
@@ -39,7 +43,42 @@ const Autocomplete = {
     }
   },
 
-  // communicate with server
+  draw: function() {
+    while (this.listUI.lastChild) {
+      this.listUI.removeChild(this.listUI.lastChild);
+    }
+
+    if (!this.visible) {
+      this.overlay.textContent = '';
+      return;
+    }
+
+    if (this.bestMatchIndex !== null && this.matches.length !== 0) {
+      var selected = this.matches[this.bestMatchIndex];
+      this.overlay.textContent = this.generateOverlayContent(this.input.value, selected);
+    } else {
+      this.overlay.textContent = '';
+    }
+
+    this.matches.forEach((match, index) => {
+      let li = document.createElement('li');
+      li.classList.add('autocomplete-ui-choice');
+
+      if (index === this.selectedIndex) {
+        li.classList.add('selected');
+        this.input.value = match.name;
+      }
+
+      li.textContent = match.name;
+      this.listUI.appendChild(li);
+    });
+  },
+
+  generateOverlayContent: function(value, match) {
+    let end = match.name.substr(value.length);
+    return value + end;
+  },
+
   fetchMatches: function(query, callback) {
     let request = new XMLHttpRequest();
 
@@ -52,57 +91,76 @@ const Autocomplete = {
     request.send();
   },
 
-  // rendering list of countries
-  draw: function() {
-    while (this.listUI.lastChild) {
-      this.listUI.removeChild(this.listUI.lastChild);
-    }
-
-    if (!this.visible) {
-      this.overlay.textContent = '';
-      return;
-    }
-
-    if (this.bestMatchIndex !== null && this.matches.length !== 0) {
-      let selected = this.matches[this.bestMatchIndex];
-      this.overlay.textContent = this.generateOverlayContent(this.input.value, selected);
-      this.overlay.textContent = selected.name;
-    } else {
-      this.overlay.textContent = '';
-    }
-
-    this.matches.forEach(match => {
-      let li = document.createElement('li');
-      li.classList.add('autocomplete-ui-choice');
-
-      li.textContent = match.name;
-      this.listUI.appendChild(li);
-    });
-  },
-
   reset: function() {
     this.visible = false;
     this.matches = [];
     this.bestMatchIndex = null;
+    this.selectedIndex = null;
+    this.previousValue = null;
 
     this.draw();
+  },
+
+  handleKeydown: function(event) {
+    switch(event.key) {
+      case 'Tab':
+        if (this.bestMatchIndex !== null && this.matches.length !== 0) {
+          this.input.value = this.matches[this.bestMatchIndex].name;
+          event.preventDefault();
+        }
+        this.reset();
+        break;
+      case 'Enter':
+        this.reset();
+        break;
+      case 'ArrowDown':
+        event.preventDefault();
+        if (this.selectedIndex === null || this.selectedIndex === this.matches.length - 1) {
+          this.selectedIndex = 0;
+        } else {
+          this.selectedIndex += 1;
+        }
+        this.bestMatchIndex = null;
+        this.draw();
+        break;
+      case 'ArrowUp':
+        event.preventDefault();
+        if (this.selectedIndex === null || this.selectedIndex === 0) {
+          this.selectedIndex = this.matches.length - 1;
+        } else {
+          this.selectedIndex -= 1;
+        }
+        this.bestMatchIndex = null;
+        this.draw();
+        break;
+      case 'Escape': // escape
+        this.input.value = this.previousValue;
+        this.reset();
+        break;
+    }
+  },
+
+  handleMousedown: function(event) {
+    let element = event.target;
+    this.input.value = element.textContent;
+    this.reset();
   },
 
   init: function() {
     this.input = document.querySelector('input');
     this.url = '/countries?matching=';
 
-    // explicitly set initial states
     this.listUI = null;
     this.overlay = null;
     this.visible = false;
-    this.matches = []
+    this.matches = [];
+    this.bestMatchIndex = null;
+    this.selectedIndex = null;
+    this.previousValue = null;
 
     this.wrapInput();
     this.createUI();
     this.bindEvents();
-
-    this.reset();
   }
 };
 
