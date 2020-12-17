fe7c1eaa98c96625a99d9da328fdcd3ee5f01633
nlicalzi <nicholas.licalzi@gmail.com>
2020-12-17 17:53:20 -0500

    completed implementation for Project: Guess a Word

--- 230-DOM_and_asynchronous_programming/putting_it_all_together/guess_a_word/javascripts/guessAWord.js
+++ 230-DOM_and_asynchronous_programming/putting_it_all_together/guess_a_word/javascripts/guessAWord.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', event => {
-  const words = ['apple', 'banana', 'orange', 'pear']; // game words
-
+  const words   = ['APPLE', 'BANANA', 'ORANGE', 'PEAR']; // game words
   const message = document.querySelector('#message');
   const letters = document.querySelector('#spaces');
   const guesses = document.querySelector('#guesses');
@@ -14,45 +13,142 @@ document.addEventListener('DOMContentLoaded', event => {
   
       let choice = choices[choices.length * Math.random() | 0]; // get rand el
       choices.splice(choices.indexOf(choice), 1);               // delete el from arr
-      return choice;
+      return choice;                                            // return el
     }
   })();
   
   class Game {
     constructor() {
       this.resetGame();
+      this.bindGuessProcessor(); // add our guess handler on keydown event listener
     }
 
-    // Set current number of incorrect guesses to 0
     resetGame() {
       this.word = this.selectNewWord();
       if (!this.word) { this.displayMessage("Sorry, I've run out of words!")};
-      this.word = this.word.split('');
+      this.targetLetters = this.word.toUpperCase().split('');
 
+      this.correctGuesses = 0;
       this.incorrectGuesses = 0;
       this.guessedLetters = [];
-      this.maxIncorrectGuesses = 6;
+      this.guesses = 6;
       this.createBlankSpaces();
+      this.hideReplayLink();
+      this.clearGuessList();
     }
 
-    displayMessage(text) { message.innerText = text; }
+    displayMessage(text) {
+      message.innerText = text;
+    }
 
-    selectNewWord() { return randomWord(); }
+    selectNewWord() {
+      return randomWord();
+    }
 
     createBlankSpaces() {
-      // get correct number of spaces
-      let spaces = (new Array(this.word.length + 1)).join('<span></span>');
-      // let spans = letters.querySelectorAll('span');
+      let spaces = (new Array(this.targetLetters.length + 1)).join('<span></span>');
+      let spans = letters.querySelectorAll('span');
 
-      // 
-      // spans.forEach(span => span.parentNode.removeChild(span));
+      spans.forEach(span => span.parentNode.removeChild(span));
       letters.insertAdjacentHTML('beforeend', spaces);
       this.spaces = document.querySelectorAll('#spaces span');
     }
+
+    processGuess(e) {
+      let letter = e.key.toUpperCase();
+
+      // guard clauses for invalid keypress / letter already guessed
+      if ((this.invalidAlphaKey(e.keyCode)) || (this.guessedLetters.includes(letter))) {
+        return; // early return
+      }
+
+      this.updateGuessList(letter);
+
+      this.correctGuess(letter) ? this.processCorrectGuess(letter) : this.processIncorrectGuess();
+    }
+
+    updateGuessList(guess) {
+      this.guessedLetters.push(guess);
+      let span = document.createElement('span');
+      span.textContent = guess;
+      guesses.appendChild(span);
+    }
+
+    clearGuessList() {
+      let spans = guesses.querySelectorAll('span');
+      spans.forEach(span => span.parentNode.removeChild(span));
+    }
+
+    populateGuessedSpaces(letter) {
+      this.targetLetters.forEach((val, idx) => {
+        if (letter === val) { this.spaces[idx].textContent = letter; }
+      }, this);
+    }
+
+    processCorrectGuess(letter) {
+      this.correctGuesses += 1;
+      this.populateGuessedSpaces(letter);
+      if (this.gameIsWon()) {
+        console.log("winning from process correct guess")
+        this.winGame();
+      }
+    }
+
+    gameIsWon() {
+      let uniqueTargetLetters = [...new Set(this.targetLetters)];
+      return (this.correctGuesses === uniqueTargetLetters.length);
+    }
+
+    processIncorrectGuess() {
+      this.incorrectGuesses += 1;
+      apples.className = 'guess_' + String(this.incorrectGuesses);
+      if (this.incorrectGuesses === 6) { this.loseGame(); }
+    }
+
+    showReplayLink() {
+      replay.hidden = false;
+    }
+
+    hideReplayLink() {
+      replay.hidden = true;
+    }
+
+    winGame() {
+      this.displayMessage("Congratulations, you guessed the word correctly!")
+      this.showReplayLink();
+      this.unbindGuessProcessor();
+    }
+
+    loseGame() {
+      this.displayMessage("Sorry, you've run out of guesses and lost!")
+      this.showReplayLink();
+      this.unbindGuessProcessor();
+    }
+
+    correctGuess(letter) {
+      return (this.targetLetters.includes(letter));
+    }
+
+    invalidAlphaKey(keyCode) { // a-z keys, inclusive
+      return !(keyCode >= 65 && keyCode <= 90);
+    }
+
+    bindGuessProcessor() {
+      // arrow func uses the `this` in lexical scope, or the Game class here
+      this.processGuessHandler = (e) => this.processGuess(e);
+      // allowing us to add an event listener that retains context properly
+      document.addEventListener('keydown', this.processGuessHandler);
+    }
+
+    unbindGuessProcessor() {
+      document.removeEventListener('keydown', this.processGuessHandler);
+    }
   }
 
-  document.addEventListener('keyup')
+  replay.addEventListener('click', (e) => {
+    e.preventDefault();
+    new Game();
+  });
 
-  let game = new Game();
+  new Game();
 });
-
