ea63ddc5e60b951a2bc2f73d1f66e15cc188cce4
nlicalzi <nicholas.licalzi@gmail.com>
2020-12-17 14:21:52 -0500

    completed part 1 of Project: Guess a Word

--- 230-DOM_and_asynchronous_programming/putting_it_all_together/guess_a_word/javascripts/guessAWord.js
+++ 230-DOM_and_asynchronous_programming/putting_it_all_together/guess_a_word/javascripts/guessAWord.js
@@ -1,11 +1,58 @@
-const WORDS = ['apple', 'banana', 'orange', 'pear']
-const randomWord = (function generateRandomWordWithClosure() {
-  let choices = [...WORDS];
-  return function() {
-    if (choices === undefined || choices.length === 0) { return undefined; }
+document.addEventListener('DOMContentLoaded', event => {
+  const words = ['apple', 'banana', 'orange', 'pear']; // game words
 
-    let choice = choices[choices.length * Math.random() | 0];
-    choices.splice(choices.indexOf(choice), 1);
-    return choice;
+  const message = document.querySelector('#message');
+  const letters = document.querySelector('#spaces');
+  const guesses = document.querySelector('#guesses');
+  const apples  = document.querySelector('#apples');
+  const replay  = document.querySelector('#replay');
+
+  const randomWord = (function() {
+    let choices = [...words];
+    return function() {
+      if (choices === undefined || choices.length === 0) { return undefined; }
+  
+      let choice = choices[choices.length * Math.random() | 0]; // get rand el
+      choices.splice(choices.indexOf(choice), 1);               // delete el from arr
+      return choice;
+    }
+  })();
+  
+  class Game {
+    constructor() {
+      this.resetGame();
+    }
+
+    // Set current number of incorrect guesses to 0
+    resetGame() {
+      this.word = this.selectNewWord();
+      if (!this.word) { this.displayMessage("Sorry, I've run out of words!")};
+      this.word = this.word.split('');
+
+      this.incorrectGuesses = 0;
+      this.guessedLetters = [];
+      this.maxIncorrectGuesses = 6;
+      this.createBlankSpaces();
+    }
+
+    displayMessage(text) { message.innerText = text; }
+
+    selectNewWord() { return randomWord(); }
+
+    createBlankSpaces() {
+      // get correct number of spaces
+      let spaces = (new Array(this.word.length + 1)).join('<span></span>');
+      // let spans = letters.querySelectorAll('span');
+
+      // 
+      // spans.forEach(span => span.parentNode.removeChild(span));
+      letters.insertAdjacentHTML('beforeend', spaces);
+      this.spaces = document.querySelectorAll('#spaces span');
+    }
   }
-})()
+
+  document.addEventListener('keyup')
+
+  let game = new Game();
+});
+
