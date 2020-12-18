0e26a0e7d8d8bba9365922badfde1f3755dd14ba
nlicalzi <nicholas.licalzi@gmail.com>
2020-12-17 21:13:08 -0500

    completed part 2 of Project: Photo Galler

--- 230-DOM_and_asynchronous_programming/putting_it_all_together/API_based_photo_gallery/public/javascripts/gallery.js
+++ 230-DOM_and_asynchronous_programming/putting_it_all_together/API_based_photo_gallery/public/javascripts/gallery.js
@@ -1,7 +1,9 @@
+let slideshow;
+
 document.addEventListener('DOMContentLoaded', event => {
   const templates = {};
   let photos;
-  
+
   document.querySelectorAll("script[type='text/x-handlebars']").forEach(tmpl => {
     templates[tmpl['id']] = Handlebars.compile(tmpl['innerHTML']);
   });
@@ -10,6 +12,53 @@ document.addEventListener('DOMContentLoaded', event => {
     Handlebars.registerPartial(tmpl['id'], tmpl['innerHTML']);
   });
 
+  class Slideshow {
+    constructor() {
+      this.slideshow = document.querySelector('#slideshow');
+      let slides = this.slideshow.querySelectorAll('figure');
+      this.firstSlide = slides[0];
+      this.lastSlide = slides[slides.length - 1];
+      this.currentSlide = this.firstSlide;
+      this.bind();
+    }
+    bind() {
+      let prevButton = this.slideshow.querySelector('a.prev');
+      let nextButton = this.slideshow.querySelector('a.next');
+      prevButton.addEventListener('click', (e) => { this.prevSlide(e) });
+      nextButton.addEventListener('click', (e) => { this.nextSlide(e) });
+    }
+    prevSlide(e) {
+      e.preventDefault();
+      let prev = this.currentSlide.previousElementSibling;
+      if (!prev) { prev = this.lastSlide; }
+      this.fadeOut(this.currentSlide);
+      this.fadeIn(prev);
+      this.renderPhotoContent(prev.getAttribute('data-id'));
+      this.currentSlide = prev;
+    }
+    nextSlide(e) {
+      e.preventDefault();
+      let next = this.currentSlide.previousElementSibling;
+      if (!next) { next = this.lastSlide; }
+      this.fadeOut(this.currentSlide);
+      this.fadeIn(next);
+      this.renderPhotoContent(next.getAttribute('data-id'));
+      this.currentSlide = next;
+    }
+    fadeOut(slide) {
+      slide.classList.add('hide');
+      slide.classList.remove('show');
+    }
+    fadeIn(slide) {
+      slide.classList.remove('hide');
+      slide.classList.add('show');
+    }
+    renderPhotoContent(idx) {
+      renderPhotoInformation(Number(idx));
+      renderComments(idx);
+    }
+  }
+
   fetch('/photos')
     .then(response => response.json())
     .then(json => {
@@ -17,11 +66,13 @@ document.addEventListener('DOMContentLoaded', event => {
       renderPhotos();
       renderPhotoInformation(photos[0].id);
       renderComments(photos[0].id);
+      slideshow = new Slideshow;
     });
 
   function renderPhotos() {
     let slides = document.getElementById('slides');
-    slides.insertAdjacentHTML('beforeend', templates.photos({ photos: photos }));
+    // slides.insertAdjacentHTML('beforeend', templates.photos({ photos: photos }));
+    slides.innerHTML = templates.photos({ photos: photos });
   }
 
   function renderPhotoInformation(idx) {
@@ -29,15 +80,17 @@ document.addEventListener('DOMContentLoaded', event => {
       return item.id === idx;
     })[0];
     let header = document.querySelector('section > header');
-    header.insertAdjacentHTML('beforeend', templates.photo_information(photo));
+    // header.insertAdjacentHTML('beforeend', templates.photo_information(photo));
+    header.innerHTML = templates.photo_information(photo);
   }
 
   function renderComments(id) {
     fetch(`/comments?photo_id=${id}`)
       .then(response => response.json())
       .then(comment_json => {
-        let comments = document.querySelector('#comments ul');
-        comments.insertAdjacentHTML('beforeend', templates.photo_comments({ comments: comment_json}));
+        let comment_list = document.querySelector('#comments ul');
+        // comment_list.insertAdjacentHTML('beforeend', templates.photo_comments({ comments: comment_json}));
+        comment_list.innerHTML = templates.photo_comments({ comments: comment_json});
       });
   }
 });
